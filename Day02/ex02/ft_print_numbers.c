f05c14331c2fd952caf1c39d8a01b76b3b3e8cc5
Mila Morozova <mmorozov@Milas-Mac.local>
2019-10-08 11:51:47 -0700

    ex02

---
+++ Day02/ex02/ft_print_numbers.c
@@ -0,0 +1,26 @@
+/*#include <unistd.h>
+  
+void    ft_putchar(int d)
+{
+        write(1, &d, 1);
+}*/
+
+void    ft_putchar(int d);
+
+void    ft_print_numbers (void)
+{
+        int a;
+
+        a = '0';
+        while (a <= '9')
+        {
+                ft_putchar(a);
+                a++;
+        }
+}
+/*
+int     main(void)
+{
+        ft_print_numbers();
+        return(0);
+}*/
