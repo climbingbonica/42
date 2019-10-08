68b642dcea632ab41a15d343f90e1d8613821057
Mila Morozova <mmorozov@Milas-Mac.local>
2019-10-08 11:32:52 -0700

    ex01

---
+++ Day02/ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+/*#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}*/
+
+void	ft_putchar(char c);
+
+void	ft_print_reverse_alphabet(void)
+{
+	int z;
+
+	z = 'z';
+	while (z >= 'a')
+	{
+		ft_putchar(z);
+		z--;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return(0);
+}*/
